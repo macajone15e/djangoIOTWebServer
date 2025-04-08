a72c1a2c95f78208cc114aa693add391cc77d4c3
massmr <marcellin.massimo@gmail.com>
2025-04-08 21:27:46 +0200

    feat : rename templates routes

--- mysite/IOT/urls.py
+++ mysite/IOT/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('devices/', views.device_list, name='device_list'),
-    path('devices/<int:device_id>/', views.viewdevice, name='viewdevice'),
+    #path('', views.home, name='home'),
+    path('', views.device_list, name='device_list'),
+    path('device/<int:device_id>/', views.viewdevice, name='viewdevice'),
 ]
